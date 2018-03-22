cab63752af6928974879fc9d7ef4620fedfb6382
Kusar Damir TCI-MW <Damir.Kusar@axpo.com>
2018-03-22 10:06:09 +0100

    changed injection of service

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,20 +1,22 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { async, TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  beforeEach(() => {
+  let service: AuthService;
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [AuthService]
     });
+  }));
+
+  beforeEach(() => {
+    service = TestBed.get(AuthService);
   });
 
-  test(
-    'should be created',
-    inject([AuthService], (service: AuthService) => {
-      expect(service).toBeTruthy();
-    })
-  );
+  test('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
